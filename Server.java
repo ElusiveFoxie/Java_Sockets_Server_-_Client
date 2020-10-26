1f71cce003edd23e290f1c3e086f641cb5cdf852
ElusiveFox <44260690+ElusiveFoxie@users.noreply.github.com>
2020-10-26 15:33:50 +0100

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,49 @@
+import java.net.*;
+import java.io.*;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
+
+    public Server(int port){
+        try {
+            serverSocket = new ServerSocket(1337);
+            socket = serverSocket.accept();
+            writer = new PrintWriter(socket.getOutputStream(), true);
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+        echo();
+    }
+    public void echo(){
+        try {
+            String response = "";
+            String line;
+
+//            while((line = reader.readLine()) != null){
+//                System.out.println(line);
+//                response += line;
+//            }
+            response = reader.readLine();
+
+            writer.println(response.toUpperCase());
+            writer.println();
+
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        new Server(1337);
+    }
+}
